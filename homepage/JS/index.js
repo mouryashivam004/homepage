c9aeaf4e963bf8613de12575db3d71c4d19f3cee
mouryashivam004 <95132864+mouryashivam004@users.noreply.github.com>
2022-01-04 10:56:57 +0530

    Update index.js

--- homepage/JS/index.js
+++ homepage/JS/index.js
@@ -1,1 +1,18 @@
+function changeBg(){
+    var scrollValue = window.scrollY;
+    // console.log(scrollValue);
+
+    var navbar = document.getElementById("navbar");
+
+    if(scrollValue < 150){
+        navbar.classList.remove('bgcolor');
+    }else{
+        navbar.classList.add('bgcolor');
+    }
+}
+
+window.addEventListener('scroll',changeBg);
+
+
+
 
